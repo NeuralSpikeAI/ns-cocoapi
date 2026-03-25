ea791d0d164168b414da8469f2e8e44de3884e88
Michal Koperski <michal@neuralspike.ai>
2026-03-25 16:54:51 +0100

    uv support (#1)
    * Package name change
    
    To avoid confusion with official package
    
    * Pyproject
    
    * Git ignore update
    
    * Added common to PythonAPI
    
    It makes uv build work
    
    * Readme
    
    * Compatibility with numpy 2.xx
    
    * Logger
    
    * Configure cibuildwheel (#2)
    
    * Patch for numpy >= 2.3
    
    * Version bump
    
    ---------
    
    Co-authored-by: Łukasz Moroz <lukaszmoroz@gmail.com>

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,27 +1,38 @@
-from setuptools import setup, Extension
-import numpy as np
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+from Cython.Build import cythonize
+import os
 
-# To compile and install locally run "python setup.py build_ext --inplace"
-# To install library to Python site-packages run "python setup.py build_ext install"
 
-ext_modules = [
+class build_ext_with_numpy(build_ext):
+    def finalize_options(self):
+        super().finalize_options()
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+common_dir = os.path.join(base_dir, "common")
+
+extensions = [
     Extension(
-        'pycocotools._mask',
-        sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
-        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        "pycocotools._mask",
+        sources=[
+#            os.path.join(common_dir, "maskApi.c"),
+            os.path.join(base_dir, "pycocotools", "_mask.pyx"),
+        ],
+        include_dirs=[common_dir],
     )
 ]
-
 setup(
-    name='pycocotools',
-    packages=['pycocotools'],
-    package_dir = {'pycocotools': 'pycocotools'},
+    name='ns-pycocotools',
+    packages=find_packages(),
     install_requires=[
         'setuptools>=18.0',
         'cython>=0.27.3',
-        'matplotlib>=2.1.0'
+        'matplotlib>=2.1.0',
+        'loguru'
     ],
-    version='2.0',
-    ext_modules= ext_modules
+    version='2.3.1',
+    cmdclass={"build_ext": build_ext_with_numpy},
+    ext_modules= extensions
 )
